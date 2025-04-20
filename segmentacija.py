1d5ad2eba85973343809bde412fae559192889b2
Tim <tim.andrejc02@gmail.com>
2025-04-20 11:07:39 +0200

    Initial commit

---
+++ segmentacija.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+def kmeans(slika, k=3, iteracije=10):
+
+    pass
+
+def meanshift(slika, velikost_okna, dimenzija):
+
+    pass
+
+def izracunaj_centre(slika, izbira, dimenzija_centra, T):
+
+    pass
+
+if __name__ == "__main__":
+    pass
